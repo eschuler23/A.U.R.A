3a63cdbb01db568952f589c9fa0435b8184a5664
ilonagreiss <31888376+ilonagreiss@users.noreply.github.com>
2025-07-03 00:01:36 +0200

    Add palette and button style

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -10,23 +10,56 @@ import { createTheme, ThemeProvider } from '@mui/material/styles'
 import AppLayout from './AppLayout'
 
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#d7d0ff',
+      dark: '#b0a7e0',
+      light: '#ebe7ff',
+    },
+    secondary: {
+      main: '#FFFFF0',
+    },
+    background: {
+      default: '#f5f4fb',
+      paper: '#ffffff',
+    },
+    text: {
+      primary: '#676775',
+    },
+  },
+  typography: {
+    fontFamily: 'Arial, sans-serif',
+  },
   components: {
     MuiCssBaseline: {
       styleOverrides: defaultTheme => ({
-        html: {
-          width: '100%',
-          height: '100%'
-        },
-        body: {
-          width: '100%',
-          height: '100%',
-          background: defaultTheme.palette.grey[200]
-        },
-        '#app': {
-          width: '100%',
+       html: {
+         width: '100%',
+         height: '100%'
+       },
+       body: {
+         width: '100%',
+         height: '100%',
+         background: defaultTheme.palette.grey[200]
+       },
+       '#app': {
+         width: '100%',
           height: '100%'
         }
       })
+    },
+    MuiButton: {
+      defaultProps: {
+        size: 'small',
+        variant: 'contained',
+        fullWidth: false,
+      },
+      styleOverrides: {
+        root: {
+          borderRadius: 15,
+          textTransform: 'none',
+        },
+      }
     }
   }
 })
